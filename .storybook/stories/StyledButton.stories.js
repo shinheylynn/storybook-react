9efe396d68564648396a49730870ed06a4306d83
shinheylynn <heylub@gmail.com>
2023-08-04 17:42:57 +0900

    Add: styled-button scss file 추가

--- .storybook/stories/StyledButton.stories.js
+++ .storybook/stories/StyledButton.stories.js
@@ -1,11 +1,130 @@
-import { Button } from '../../src/components/styled-button/medistream';
+import React from 'react';
+import { StyledButton } from '../../src/components/styled-button/medistream';
 
-export default {
-  component: Button,
+const Playground = (props) => React.createElement(StyledButton, props);
+Playground.args = {
+  width: 'auto',
+  size: 'medium',
+  color: 'primary',
+  outlined: false,
+  disabled: false,
+  loading: false,
 };
 
-export const Primary = {
-  args: {
-    variant: 'primary',
+const size = (props) => React.createElement(StyledButton, { size: props.size });
+size.args = {
+  size: 'x-large',
+};
+
+const width = (props) => React.createElement(StyledButton, { width: props.width });
+width.args = {
+  width: 200,
+};
+
+const color = (props) => React.createElement(StyledButton, { color: props.color });
+color.args = {
+  color: 'secondary',
+};
+
+const outlined = (props) => React.createElement(StyledButton, { outlined: props.outlined });
+outlined.args = {
+  outlined: true,
+};
+
+const disabled = (props) => React.createElement(StyledButton, { disabled: props.disabled });
+disabled.args = {
+  disabled: true,
+};
+
+const loading = (props) => React.createElement(StyledButton, { loading: props.loading });
+loading.args = {
+  loading: true,
+};
+
+// Prop 값 타입과 입력 UI 를 정의합니다.
+const ownArgTypes = {
+  width: {
+    control: {
+      type: 'number',
+    },
   },
+  size: {
+    control: {
+      type: 'inline-radio',
+    },
+    options: ['x-small', 'small', 'medium', 'large', 'x-large'],
+  },
+  color: {
+    control: {
+      type: 'inline-radio',
+    },
+    options: ['primary', 'secondary', 'grey', 'yellow'],
+  },
+  outlined: {
+    control: {
+      type: 'boolean',
+    },
+  },
+  disabled: {
+    control: {
+      type: 'boolean',
+    },
+  },
+  loading: {
+    control: {
+      type: 'boolean',
+    },
+  },
+  Type: {
+    control: {
+      type: 'inline-radio',
+    },
+    options: ['button', 'submit', 'reset'],
+  },
+  // 사용하지 않을 Prop의 경우 아래와 같이 설정해줍니다.
+  // View에 영향을 끼치지 않거나 변경하지 않을 Prop을 control과 table에서 사용하지 않도록 해줍니다.
+  to: {
+    table: {
+      disable: true,
+    },
+    control: false,
+  },
+  HeightUnit: {
+    table: {
+      disable: true,
+    },
+    control: false,
+  },
+  PaddingUnit: {
+    table: {
+      disable: true,
+    },
+    control: false,
+  },
+  FontSizeUnit: {
+    table: {
+      disable: true,
+    },
+    control: false,
+  },
+  FontWeightUnit: {
+    table: {
+      disable: true,
+    },
+    control: false,
+  },
+  BorderRadiusUnit: {
+    table: {
+      disable: true,
+    },
+    control: false,
+  },
+};
+
+export { Playground, size, width, color, outlined, disabled, loading };
+
+export default {
+  title: 'Controls/StyledButton',
+  component: StyledButton,
+  argTypes: { ...ownArgTypes },
 };
--- src/components/styled-button/medistream.jsx
+++ src/components/styled-button/medistream.jsx
@@ -1,3 +1,155 @@
-export const Button = () => {
-  return <button>button</button>;
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../colors/medistream.variables.scss';
+import './medistream.scss';
+
+export const StyledButton = ({
+  width = 'auto',
+  size = 'medium',
+  color = 'primary',
+  outlined = false,
+  disabled = false,
+  loading = false,
+  Type = 'button',
+  to = null,
+  HeightUnit = [24, 28, 36, 44, 52],
+  PaddingUnit = [8, 8, 10, 14, 14],
+  FontSizeUnit = [12, 13, 14, 16, 16],
+  FontWeightUnit = [600, 600, 700, 700, 700],
+  BorderRadiusUnit = [4, 4, 6, 6, 6],
+  children,
+}) => {
+  const widthValue = typeof width === 'number' ? `${width}px` : width === 'auto' ? null : width;
+
+  const heightValue = (() => {
+    switch (size) {
+      case 'x-large':
+        return HeightUnit[4];
+      case 'large':
+        return HeightUnit[3];
+      case 'small':
+        return HeightUnit[1];
+      case 'x-small':
+        return HeightUnit[0];
+      case 'medium':
+      default:
+        return HeightUnit[2];
+    }
+  })();
+
+  const paddingHorizontal = (() => {
+    switch (size) {
+      case 'x-large':
+        return PaddingUnit[4];
+      case 'large':
+        return PaddingUnit[3];
+      case 'small':
+        return PaddingUnit[1];
+      case 'x-small':
+        return PaddingUnit[0];
+      case 'medium':
+      default:
+        return PaddingUnit[2];
+    }
+  })();
+
+  const fontSize = (() => {
+    switch (size) {
+      case 'x-large':
+        return FontSizeUnit[4];
+      case 'large':
+        return FontSizeUnit[3];
+      case 'small':
+        return FontSizeUnit[1];
+      case 'x-small':
+        return FontSizeUnit[0];
+      case 'medium':
+      default:
+        return FontSizeUnit[2];
+    }
+  })();
+
+  const fontWeight = (() => {
+    switch (size) {
+      case 'x-large':
+        return FontWeightUnit[4];
+      case 'large':
+        return FontWeightUnit[3];
+      case 'small':
+        return FontWeightUnit[1];
+      case 'x-small':
+        return FontWeightUnit[0];
+      case 'medium':
+      default:
+        return FontWeightUnit[2];
+    }
+  })();
+
+  const borderRadius = (() => {
+    switch (size) {
+      case 'x-large':
+        return BorderRadiusUnit[4];
+      case 'large':
+        return BorderRadiusUnit[3];
+      case 'small':
+        return BorderRadiusUnit[1];
+      case 'x-small':
+        return BorderRadiusUnit[0];
+      case 'medium':
+      default:
+        return BorderRadiusUnit[2];
+    }
+  })();
+
+  const buttonClassNames = [
+    'styled-button',
+    color,
+    size,
+    outlined ? 'outlined' : '',
+    disabled ? 'disabled' : '',
+    loading ? 'loading' : '',
+    'dark',
+  ].join(' ');
+
+  const commonButtonStyle = {
+    height: `${heightValue}px`,
+    padding: `0 ${paddingHorizontal}px`,
+    fontSize: `${fontSize}px`,
+    fontWeight: fontWeight,
+    borderRadius: `${borderRadius}px`,
+  };
+
+  const handleClick = (e) => {
+    if (!disabled && !loading) {
+      if (to) {
+        console.log('Navigating to:', to);
+      } else {
+        console.log('Button clicked!');
+      }
+    }
+  };
+
+  return (
+    <span className={buttonClassNames} style={{ width: widthValue }}>
+      {to ? (
+        <Link
+          to={to}
+          style={{
+            height: `${heightValue}px`,
+            lineHeight: `${heightValue - 2}px`,
+            ...commonButtonStyle,
+          }}>
+          {loading ? 'Loading...' : <>{children}</>}
+        </Link>
+      ) : (
+        <button
+          type={Type}
+          disabled={disabled || loading}
+          style={{ height: `${heightValue}px`, ...commonButtonStyle }}
+          onClick={handleClick}>
+          {loading ? 'Loading...' : <>{children || 'button'}</>}
+        </button>
+      )}
+    </span>
+  );
 };
